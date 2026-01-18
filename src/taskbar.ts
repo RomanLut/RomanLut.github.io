af6697d568e10d1c6b97e01a6bd3d6fce64d0caa
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2026-01-18 10:02:17 +0100

    fixed icons

--- src/taskbar.ts
+++ src/taskbar.ts
@@ -18,10 +18,18 @@ export class Taskbar {
           <path d="M3 9v6h4l5 4V5L7 9H3z" fill="currentColor" />
         </svg>
         <svg class="tray-icon" viewBox="0 0 24 24" aria-hidden="true">
-          <path d="M4 9v6c0 2.2 1.8 4 4 4h2c2.2 0 4-1.8 4-4V9c0-2.2-1.8-4-4-4H8c-2.2 0-4 1.8-4 4zm13.5-4.5c.8 0 1.5.7 1.5 1.5v9c0 .8-.7 1.5-1.5 1.5h-1v-12h1z" fill="currentColor" />
+          <path
+            fill-rule="evenodd"
+            d="M3 8a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v1h1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-1v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8Zm2 0a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H5Z"
+            fill="currentColor"
+          />
+          <rect x="5" y="9.5" width="9" height="5" rx="1" fill="currentColor" opacity="0.9" />
         </svg>
-        <svg class="tray-icon" viewBox="0 0 24 24" aria-hidden="true">
-          <path d="M4 12a8 8 0 0 1 8-8v3a5 5 0 0 0-5 5H4zm8-6a10 10 0 0 1 10 10h-3a7 7 0 0 0-7-7V6zm0 6a4 4 0 0 1 4 4h-3a1 1 0 0 0-1-1v-3z" fill="currentColor" />
+        <svg class="tray-icon tray-icon--wifi" viewBox="0 0 24 24" aria-hidden="true">
+          <path
+            d="M4 11.5c4.7-4.1 11.3-4.1 16 0l-1.6 1.8c-3.5-3-9.3-3-12.8 0L4 11.5zm3 3.2c2.9-2.5 7.1-2.5 10 0l-1.6 1.8c-1.9-1.6-4.9-1.6-6.8 0L7 14.7zm3 3.2c1.2-1 2.8-1 4 0l-2 2.1-2-2.1zm1.9 4.6a1.4 1.4 0 1 0 0-2.9 1.4 1.4 0 0 0 0 2.9z"
+            fill="currentColor"
+          />
         </svg>
       </div>
       <div class="taskbar__clock">
